283c766902e3d285838722a6fb10d2a28d2ea6ff
Cilooth <37047157+Cilooth@users.noreply.github.com>
2024-04-14 22:24:36 +0200

    Update staff-stats.py

--- staff-stats/staff-stats.py
+++ staff-stats/staff-stats.py
@@ -1,15 +1,12 @@
 import asyncio
-
 from discord.ext import commands
-
 from core import checks
 from core.models import PermissionLevel
 
-
 class StaffStatsPlugin(commands.Cog):
     """
-  Just a plugin which saves staff statistics in the database for frontend stuff.
-  """
+    Just a plugin which saves staff statistics in the database for frontend stuff.
+    """
 
     def __init__(self, bot):
         self.bot = bot
@@ -18,34 +15,35 @@ class StaffStatsPlugin(commands.Cog):
 
     async def _update_stats(self):
         while True:
-            staff_list = list()
+            staff_list = []
+            category_id = self.bot.config.get("main_category_id")
+            if category_id is None:
+                await asyncio.sleep(86400)
+                continue
 
-            category = self.bot.get_channel(
-                int(self.bot.config.get("main_category_id"))
-            )
+            category = self.bot.get_channel(int(category_id))
+            if category is None:
+                await asyncio.sleep(86400)
+                continue
 
-            # L28 - L30 taken from https://github.com/papiersnipper/modmail-plugins/blob/daa6e31356030cb35ef12e4cf0e1df7015065798/supporters/supporters.py#L47-L49
             for member in self.bot.modmail_guild.members:
-                if member.permissions_in(category).read_messages:
-                    if not member.bot:
-                        responded = await self.bot.api.get_responded_logs(member.id)
-                        closed = await self.bot.db.logs.find(
-                            {
-                                "guild_id": str(self.bot.guild_id),
-                                "open": False,
-                                "closer.id": str(member.id),
-                            },
-                            {"messages": {"$slice": 5}},
-                        ).to_list(None)
-                        staff_list.append(
-                            {
-                                "username": str(member),
-                                "id": member.id,
-                                "closed": len(tuple(closed)),
-                                "responded": len(tuple(responded)),
-                                "avatar": str(member.avatar_url),
-                            }
-                        )
+                if category.permissions_for(member).read_messages and not member.bot:
+                    responded = await self.bot.api.get_responded_logs(member.id)
+                    closed = await self.bot.db.logs.find(
+                        {
+                            "guild_id": str(self.bot.guild_id),
+                            "open": False,
+                            "closer.id": str(member.id),
+                        }
+                    ).to_list(None)
+
+                    staff_list.append({
+                        "username": str(member),
+                        "id": member.id,
+                        "closed": len(closed),
+                        "responded": len(responded),
+                        "avatar": str(member.avatar_url),
+                    })
 
             await self.db.find_one_and_update(
                 {"_id": "config"}, {"$set": {"staff": staff_list}}, upsert=True
@@ -56,36 +54,35 @@ class StaffStatsPlugin(commands.Cog):
     @commands.command()
     @checks.has_permissions(PermissionLevel.ADMIN)
     async def syncstaff(self, ctx):
-        """
-         Sync Staff
-        """
+        staff_list = []
+        category_id = self.bot.config.get("main_category_id")
+        if category_id is None:
+            await ctx.send("Configuration error: main category ID is missing.")
+            return
 
-        staff_list = list()
+        category = self.bot.get_channel(int(category_id))
+        if category is None:
+            await ctx.send("Error: The specified channel does not exist.")
+            return
 
-        category = self.bot.get_channel(int(self.bot.config.get("main_category_id")))
-
-        # L68 - L70 taken from https://github.com/papiersnipper/modmail-plugins/blob/daa6e31356030cb35ef12e4cf0e1df7015065798/supporters/supporters.py#L47-L49
         for member in self.bot.modmail_guild.members:
-            if member.permissions_in(category).read_messages:
-                if not member.bot:
-                    responded = await self.bot.api.get_responded_logs(member.id)
-                    closed = await self.bot.db.logs.find(
-                        {
-                            "guild_id": str(self.bot.guild_id),
-                            "open": False,
-                            "closer.id": str(member.id),
-                        },
-                        {"messages": {"$slice": 5}},
-                    ).to_list(None)
-                    staff_list.append(
-                        {
-                            "username": str(member),
-                            "id": member.id,
-                            "closed": len(tuple(closed)),
-                            "responded": len(tuple(responded)),
-                            "avatar": str(member.avatar_url),
-                        }
-                    )
+            if category.permissions_for(member).read_messages and not member.bot:
+                responded = await self.bot.api.get_responded_logs(member.id)
+                closed = await self.bot.db.logs.find(
+                    {
+                        "guild_id": str(self.bot.guild_id),
+                        "open": False,
+                        "closer.id": str(member.id),
+                    }
+                ).to_list(None)
+
+                staff_list.append({
+                    "username": str(member),
+                    "id": member.id,
+                    "closed": len(closed),
+                    "responded": len(responded),
+                    "avatar": str(member.avatar_url),
+                })
 
         await self.db.find_one_and_update(
             {"_id": "config"}, {"$set": {"staff": staff_list}}, upsert=True
@@ -93,7 +90,6 @@ class StaffStatsPlugin(commands.Cog):
 
         await ctx.send("Done.")
         return
-
 
 def setup(bot):
     bot.add_cog(StaffStatsPlugin(bot))
